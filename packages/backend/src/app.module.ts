b5c5827e0586ae248323d688825eb166af6a7c02
FearsomeRover <bujgergo@gmail.com>
2023-05-11 19:36:09 +0200

    added links prisma schema

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { LinksModule } from './links/links.module'
 @Module({
-  imports: [],
+  imports: [LinksModule],
   controllers: [AppController],
   providers: [AppService],
 })
