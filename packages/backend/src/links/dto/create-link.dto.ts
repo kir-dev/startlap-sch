7e7ce0e0ea0f33b963971a60ccc557bc082c1e2e
Dkrisztan <krisztidancs@gmail.com>
2023-05-16 17:29:38 +0200

    link service methods implemented

--- packages/backend/src/links/dto/create-link.dto.ts
+++ packages/backend/src/links/dto/create-link.dto.ts
@@ -1,1 +1,31 @@
-export class CreateLinkDto {}
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString, IsUUID, IsUrl } from 'class-validator';
+
+export class CreateLinkDto {
+  @IsUUID()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  slug: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  iconUrl: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  keywords: string;
+}
