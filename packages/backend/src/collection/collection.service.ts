8ba73a0271f03d82ac7f279b2af560b84b5965eb
K155 <gulyasbence.dev@gmail.com>
2023-05-23 22:24:18 +0200

    Edited collection service methods

--- packages/backend/src/collection/collection.service.ts
+++ packages/backend/src/collection/collection.service.ts
@@ -1,22 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCollectionDto } from './dto/CreateCollection.dto';
 import { UpdateCollectionDto } from './dto/UpdateCollection.dto';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class CollectionService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createCollectionDto: CreateCollectionDto) {
-    return 'CreateCollectionDto';
+    return this.prisma.collection.create({ data: createCollectionDto });
   }
   findAll() {
-    return 'findAll';
+    return this.prisma.collection.findMany();
   }
-  findOne(id: string) {
-    return 'findAll';
+  async findOne(id: string) {
+    const collection = await this.prisma.collection.findUnique({
+      where: { id },
+    });
+    if (collection === null) {
+      throw new NotFoundException('Collection not found');
+    }
+    return collection;
   }
   update(id: string, updateCollectionDto: UpdateCollectionDto) {
-    return 'updateCollectionDto';
+    return this.prisma.collection.update({ where: { id }, data: updateCollectionDto });
   }
   remove(id: string) {
-    return 'remove';
+    return this.prisma.collection.delete({ where: { id } });
   }
 }
