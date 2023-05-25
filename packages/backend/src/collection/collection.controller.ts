3c065abcc006ef53dd0435834d79957514d435b8
Saskó Máté <sasko.mate.03@gmail.com>
2023-05-25 19:09:26 +0200

    added endpoints

--- packages/backend/src/collection/collection.controller.ts
+++ packages/backend/src/collection/collection.controller.ts
@@ -1,25 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Delete, Get, Patch, Param, Body } from '@nestjs/common';
+import { CollectionService } from './collection.service';
 @Controller('collection')
 export class CollectionController {
-  constructor() {}
+  constructor(private readonly collectionService: CollectionService) {}
   @Get()
   findAll() {
-    return '';
+    return this.collectionService.findAll();
   }
 
   @Get(':id')
-  findOne() {
-    return '';
+  findOne(@Param('id') id: string) {
+    return this.collectionService.findOne(id);
   }
 
   @Patch(':id')
-  update() {
-    return '';
+  update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
+    return this.collectionService.update(id, updateLinkDto);
   }
 
   @Delete(':id')
-  remove() {
-    return '';
+  remove(@Param('id') id: string) {
+    return this.collectionService.remove(id);
   }
 }
