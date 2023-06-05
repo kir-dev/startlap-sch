ddd52b2e4c1fb37e2d76384f2d1ee03ec435f8c3
FearsomeRover <bujgergo@gmail.com>
2023-06-05 15:11:15 +0200

    added slug verification endpoint

--- packages/backend/src/links/links.controller.ts
+++ packages/backend/src/links/links.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { LinksService } from './links.service';
-import { CreateLinkDto } from './dto/create-link.dto';
-import { UpdateLinkDto } from './dto/update-link.dto';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { CreateLinkDto } from './dto/create-link.dto'
+import { UpdateLinkDto } from './dto/update-link.dto'
+import { LinksService } from './links.service'
 
 @Controller('links')
 export class LinksController {
@@ -9,26 +9,30 @@ export class LinksController {
 
   @Post()
   create(@Body() createLinkDto: CreateLinkDto) {
-    return this.linksService.create(createLinkDto);
+    return this.linksService.create(createLinkDto)
   }
 
   @Get()
   findAll() {
-    return this.linksService.findAll();
+    return this.linksService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.linksService.findOne(id);
+    return this.linksService.findOne(id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
-    return this.linksService.update(id, updateLinkDto);
+    return this.linksService.update(id, updateLinkDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.linksService.remove(id);
+    return this.linksService.remove(id)
+  }
+  @Get(':slug')
+  chechSlug(@Param('slug') slug: string) {
+    return this.linksService.checkSlug(slug)
   }
 }
