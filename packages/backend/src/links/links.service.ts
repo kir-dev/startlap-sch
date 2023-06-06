c8c777bed73ac9c7a4e1c8f0435a2ee2486625dd
Gergő Bujdosó <bujgergo@gmail.com>
2023-06-06 09:59:42 +0200

    Feature/41 slug verification (#46)
    * added slug verification endpoint

--- packages/backend/src/links/links.service.ts
+++ packages/backend/src/links/links.service.ts
@@ -1,45 +1,54 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { CreateLinkDto } from './dto/create-link.dto';
-import { UpdateLinkDto } from './dto/update-link.dto';
-import { PrismaService } from 'nestjs-prisma';
-import { Prisma } from '@prisma/client';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { PrismaService } from 'nestjs-prisma'
+import { CreateLinkDto } from './dto/create-link.dto'
+import { slugAvailable } from './dto/slug-verification.dto'
+import { UpdateLinkDto } from './dto/update-link.dto'
 
 @Injectable()
 export class LinksService {
   constructor(private readonly prisma: PrismaService) {}
   async create(createLinkDto: CreateLinkDto) {
     try {
-      return await this.prisma.link.create({ data: createLinkDto });
+      return await this.prisma.link.create({ data: createLinkDto })
     } catch (e) {
       if (e instanceof Prisma.PrismaClientKnownRequestError) {
         if (e.code === 'P2002') {
-          throw new BadRequestException('This slug violates the unique constraint!');
+          throw new BadRequestException('This slug violates the unique constraint!')
         }
       }
     }
   }
 
   findAll() {
-    return this.prisma.link.findMany();
+    return this.prisma.link.findMany()
   }
 
   async findOne(id: string) {
     const link = await this.prisma.link.findUnique({
       where: { id },
-    });
+    })
 
     if (link === null) {
-      throw new NotFoundException('The link you entered is not found!');
+      throw new NotFoundException('The link you entered is not found!')
     }
 
-    return link;
+    return link
   }
 
   async update(id: string, updateLinkDto: UpdateLinkDto) {
-    return await this.prisma.link.update({ where: { id }, data: updateLinkDto });
+    return await this.prisma.link.update({ where: { id }, data: updateLinkDto })
   }
 
   async remove(id: string) {
-    return await this.prisma.link.delete({ where: { id } });
+    return await this.prisma.link.delete({ where: { id } })
+  }
+  async checkSlug(slug: string): Promise<slugAvailable> {
+    const link = await this.prisma.link.findUnique({
+      where: { slug },
+    })
+    return {
+      available: link === null,
+    }
   }
 }
