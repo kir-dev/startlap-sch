c51caba01b3fd64b826cd61eed033ead13ece2e4
FearsomeRover <bujgergo@gmail.com>
2023-06-09 15:55:32 +0200

    added yaml export

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -4,6 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { PrismaService } from 'nestjs-prisma'
 import { AppModule } from './app.module'
 import { BACKEND_PORT } from './util/environment'
+const yaml = require('yaml')
+const fs = require('fs')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -18,11 +20,11 @@ async function bootstrap() {
   )
   const config = new DocumentBuilder().setTitle('Startlap SCH').setDescription('The Startlap SCH API description').setVersion('1.0').build()
   const document = SwaggerModule.createDocument(app, config)
+  fs.writeFileSync('./openapi.yaml', yaml.stringify(document, {}))
   SwaggerModule.setup('api', app, document)
 
   const prismaService: PrismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
-
   await app.listen(BACKEND_PORT)
 }
 bootstrap()
