a9882783d2b9abccb21bc832e609e76b992ca301
FearsomeRover <bujgergo@gmail.com>
2023-06-09 14:36:51 +0200

    fixed swagger, added examples to link dto

--- packages/backend/src/links/entities/link.entity.ts
+++ packages/backend/src/links/entities/link.entity.ts
@@ -1,30 +1,50 @@
+import { ApiHideProperty } from '@nestjs/swagger'
 import { ArrayMinSize, IsArray, IsNotEmpty, IsOptional, IsString, IsUUID, IsUrl } from 'class-validator'
 
 export class Link {
+  @ApiHideProperty()
   @IsUUID()
   id: string
-
+  /**
+   * The url to the link
+   * @example 'https://example.com'
+   */
   @IsString()
   @IsNotEmpty()
   @IsUrl()
   url: string
-
+  /**
+   * The title of the link
+   * @example 'Example Link'
+   */
   @IsString()
   @IsNotEmpty()
   title: string
-
+  /**
+   * The slug corresponding to the link
+   * @example 'example-link'
+   */
   @IsString()
   @IsNotEmpty()
   slug: string
-
+  /**
+   * The description corresponding to the link
+   * @example 'This is an example link'
+   */
   @IsString()
   @IsNotEmpty()
   description: string
-
+  /**
+   * The url to the icon corresponding to the link
+   * @example 'https://example.com/icon.png'
+   */
   @IsString()
   @IsOptional()
   iconUrl: string
-
+  /**
+   * The url to the icon corresponding to the link
+   * @example ["example","link"]
+   */
   @IsArray()
   @IsString({ each: true })
   @ArrayMinSize(1)
