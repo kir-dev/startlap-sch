09ea8d4f3a4be4c3c3a3891bcfe4b8929f5d0203
Saskó Máté <61990954+MateVagyokK@users.noreply.github.com>
2023-06-17 18:24:07 +0200

    Feature/collection controller (#44)
    * Added collection controller skeleton
    
    * added endpoints
    
    * feat: finished collection feature
    
    ---------
    
    Co-authored-by: Bálint Berente <30603208+berenteb@users.noreply.github.com>

--- packages/backend/src/collection/collection.module.ts
+++ packages/backend/src/collection/collection.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common'
-
+import { Module } from '@nestjs/common';
+import { CollectionController } from './collection.controller';
 import { CollectionService } from './collection.service';
 
 @Module({
-  controllers: [],
+  controllers: [CollectionController],
   providers: [CollectionService],
 })
 export class CollectionModule {}
