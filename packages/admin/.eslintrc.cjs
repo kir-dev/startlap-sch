fd074ed4339ac2e1433a3f75498fe62f1fde5dcc
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-06-27 22:29:50 +0200

    feat: initialized frontend packages (#54)
    * feat: initialized frontend packages
    
    * yarn dev in every package
    
    * root yarn dev to start all servers
    
    * chore: lockfile
    
    ---------
    
    Co-authored-by: Sámuel Fekete <feketesamu@gmail.com>

---
+++ packages/admin/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "prettier", "simple-import-sort"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
+  },
+};
