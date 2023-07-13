1cd26869f68b631eb3526808d0662fc85533a292
Sámuel Fekete <feketesamu@gmail.com>
2023-07-13 14:32:31 +0200

    optional icon file, custom validators to delete file

--- packages/backend/src/util/FileMaxSizeValidator.ts
+++ packages/backend/src/util/FileMaxSizeValidator.ts
@@ -3,14 +3,12 @@ import { unlink } from 'fs'
 import { join } from 'path'
 
 export class FileMaxSizeValidator extends FileValidator<{ maxSize: number }> {
-  //isValid(file?: Express.Multer.File): boolean | Promise<boolean> {
-  // TODO this worked with the Express type until nest v10
-  isValid(file?: any): boolean | Promise<boolean> {
+  isValid(file?: Express.Multer.File): boolean {
     return file.size <= this.validationOptions.maxSize
   }
 
   buildErrorMessage(file: Express.Multer.File): string {
     unlink(join(process.cwd(), '/static', file.filename), () => {})
-    throw new PayloadTooLargeException(`A fájl mérete maximum ${this.validationOptions.maxSize / 1000000} MB lehet!`)
+    throw new PayloadTooLargeException(`The size of the file can't exceed ${this.validationOptions.maxSize / 1000000} MB!`)
   }
 }
