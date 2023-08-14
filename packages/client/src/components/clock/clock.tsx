35b5c6f86004a210b88e118c9d4074e305d01ab2
FearsomeRover <bujgergo@gmail.com>
2023-08-14 21:43:13 +0200

    added working date, clock

--- packages/client/src/components/clock/clock.tsx
+++ packages/client/src/components/clock/clock.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Clock() {
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+  const datestr = `${dateState
+    .toLocaleDateString("hu-HU", {
+      dateStyle: "short",
+    })
+    .split(" ")
+    .join("")}
+  ${dateState.toLocaleDateString("hu-HU", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+  })}`;
   return (
-    <div className="z-50 block flex w-full justify-center pt-4 ">
+    <div className="z-50 flex w-full justify-center pt-4 ">
       <div className="m-1 w-fit">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -13,12 +28,12 @@ export default function Clock() {
           <path d="M180-80q-24 0-42-18t-18-42v-620q0-24 18-42t42-18h65v-60h65v60h340v-60h65v60h65q24 0 42 18t18 42v620q0 24-18 42t-42 18H180Zm0-60h600v-430H180v430Zm0-490h600v-130H180v130Zm0 0v-130 130Zm300 230q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" />
         </svg>
       </div>
-      <div className=" w-fit flex-col   ">
-        <div className=" w-fit ">
-          <h5 className="text-base">2023.07.20. Thursday 09:54</h5>
+      <div className=" w-fit flex-col">
+        <div className=" w-fit">
+          <h5 className="text-base">{datestr}</h5>
         </div>
         <div className="flex w-fit">
-          <div className="   ">
+          <div>
             <h5 className="text-base text-gray-500">1969.07.20. Sunday</h5>
           </div>
           <div className="w-40  ">
