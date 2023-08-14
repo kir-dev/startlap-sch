bef767686bb498dca97a04b153f9340aa5ab5653
FearsomeRover <bujgergo@gmail.com>
2023-08-14 19:14:22 +0200

    slug verification in collection, fixed swagger

--- packages/backend/src/collection/dto/CreateCollection.dto.ts
+++ packages/backend/src/collection/dto/CreateCollection.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types'
+import { PickType } from '@nestjs/swagger'
 import { CollectionEntity } from './CollectionEntity.dto'
 
 export class CreateCollectionDto extends PickType(CollectionEntity, ['iconUrl', 'bannerUrl', 'slug', 'name']) {}
