d2eabbfdb52b14693c0d87260e04101c386bc03b
Sámuel Fekete <feketesamu@gmail.com>
2023-08-15 22:20:21 +0200

    Icon upload (#71)
    * first draft
    
    * optional icon file, custom validators to delete file
    
    * delete if validation fails, patch endpoint
    
    * first draft
    
    * optional icon file, custom validators to delete file
    
    * delete if validation fails, patch endpoint
    
    * add swagger support
    
    * cleaner solution: extend BaseExceptionFilter
    
    * fix dto type
    
    * fix conflict
    
    ---------
    
    Co-authored-by: Sámuel Fekete <samuel.fekete@cae.com>

---
+++ packages/backend/src/util/DeleteFileExceptionFilter.ts
@@ -0,0 +1,22 @@
+import { ArgumentsHost, Catch } from '@nestjs/common'
+import { BaseExceptionFilter } from '@nestjs/core'
+import { Request } from 'express'
+import { unlink } from 'fs'
+import { join } from 'path'
+
+/**
+ * If a file was passed with the request as form-data,
+ * it will be deleted from the static folder before
+ * passing the exception to the base exception handler of Nest.
+ */
+@Catch()
+export class DeleteFileExceptionFilter extends BaseExceptionFilter {
+  catch(exception: any, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp()
+    const request = ctx.getRequest<Request>()
+    if (request.file) {
+      unlink(join(process.cwd(), '/static', request.file.filename), () => {})
+    }
+    super.catch(exception, host)
+  }
+}
