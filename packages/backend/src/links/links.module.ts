56e15358e983808321c04c342700f2acabcc5927
Marcell Nemeth <79668706+nmarci7@users.noreply.github.com>
2024-02-11 11:12:38 +0100

    Creating url validation for submissions, with Samu (#100)
    * Creating url validation for submissions, with Samu
    
    * Prettier

--- packages/backend/src/links/links.module.ts
+++ packages/backend/src/links/links.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { PrismaModule } from 'nestjs-prisma'
 import { LinksController } from './links.controller'
 import { LinksService } from './links.service'
+import { Link } from './entities/link.entity'
 
 @Module({
   controllers: [LinksController],
   providers: [LinksService],
   imports: [PrismaModule],
+  exports: [LinksService],
 })
 export class LinksModule {}
