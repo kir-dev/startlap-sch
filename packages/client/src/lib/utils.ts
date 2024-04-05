dfb494dd26537e548ce3b57447728f5015242d84
Tamas Sudi <57364372+tomitheninja@users.noreply.github.com>
2024-04-05 21:13:08 +0200

    Refactor clock component

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { type ClassValue, clsx } from 'clsx'
+import axios from 'axios'
+import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export interface FunFact {
+  year: number
+  text: string
+}
+
+export async function funFactOfTheDay(date: Date): Promise<FunFact> {
+  const FUN_FACT_API_BASE = 'https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all'
+  const response = await axios.get(`${FUN_FACT_API_BASE}/${date.getMonth() + 1}/${date.getDate()}`)
+
+  const chosenEvent = response.data.events[Math.floor(Math.random() * response.data.events.length)]
+  return {
+    text: chosenEvent.text,
+    year: chosenEvent.year,
+  }
+}
+
+export function fillWith0(input: number, neededLength: number) {
+  if (input.toString().length >= neededLength) return input.toString()
+
+  const addedCount = neededLength - input.toString().length
+  let answer = ''
+  for (let i = 0; i < addedCount; i++) {
+    answer += '0'
+  }
+  answer += input.toString()
+
+  return answer
+}
+
