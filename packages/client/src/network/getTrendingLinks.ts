8414dc3306591aeb95f2d167792c14b08ee825c2
Horváth István <horvath.isti@edu.bme.hu>
2024-04-05 17:10:21 +0200

    Set cache lifetime

--- packages/client/src/network/getTrendingLinks.ts
+++ packages/client/src/network/getTrendingLinks.ts
@@ -2,7 +2,7 @@ import { LinkWithVisitsEntity } from '@/types/link.type'
 
 export async function getTrendingLinks(): Promise<LinkWithVisitsEntity[]> {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/links/trending`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/links/trending`, { next: { revalidate: 300 } })
     return await response.json()
   } catch (e) {
     console.error(e)
