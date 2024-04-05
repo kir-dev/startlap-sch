57b170be0b37daaf4b13081dbc42aa5db29b3bad
Bujdosó Gergő <bujgergo@gmail.com>
2024-04-05 16:25:46 +0200

    Added favorites

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common'
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
-import { User, UserRole } from '@prisma/client'
+import { Link, User, UserRole } from '@prisma/client'
 import { CurrentUser } from 'src/auth/decorators/CurrentUser.decorator'
 import { JwtAuth } from 'src/auth/decorators/JwtAuth'
 import { Roles } from 'src/auth/decorators/Roles.decorator'
@@ -23,5 +23,14 @@ export class UserController {
   @Patch(':id/role')
   changeRole(@Param('id') id: string, @Body() role: ChangeUserRole): Promise<User> {
     return this.userService.updateRole(id, role)
+  }
+
+  @Post('/favorites/:id')
+  favorite(@Param('id') id: string, @CurrentUser() user: User): Promise<void> {
+    return this.userService.saveFavorite(id, user)
+  }
+  @Get('/favorites')
+  async favorites(@CurrentUser() user: User): Promise<Link[]> {
+    return this.userService.getFavorites(user)
   }
 }
