71aad1d79ed2e401e9814094356154958f5f8a82
DannyS123 <szdani2012@gmail.com>
2024-04-05 17:20:24 +0200

    better layout with basic footer

---
+++ packages/client/src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Footer() {
+  return (
+    <footer className='sticky top-0 border bg-white px-6 py-3'>
+      <div className='flex flex-col text-center'>
+        <p>Made with ♥ by Kir-Dev</p>
+        <p>2024</p>
+      </div>
+    </footer>
+  )
+}
