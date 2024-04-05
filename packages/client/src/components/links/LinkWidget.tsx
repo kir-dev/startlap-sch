8bec88948675f27d37f6aa79cefbd060e84a46fc
Balog Bors <144214912+balog-b@users.noreply.github.com>
2024-04-05 17:30:21 +0200

    Feat/tracking (#118)
    * feat: Link opens links/visit path
    
    * + use client
    
    * + title

--- packages/client/src/components/links/LinkWidget.tsx
+++ packages/client/src/components/links/LinkWidget.tsx
@@ -1,6 +1,9 @@
+'use client';
+import "../../app/globals.css";
 import '../../app/globals.css'
 
-import Link from 'next/link'
+import Link from "next/link";
+import React from "react";
 
 import { LinkEntity } from '@/types/link.type'
 
@@ -9,9 +12,22 @@ interface Props {
 }
 
 export default function LinkWidget(props: Props) {
-  const link = props.link
+  const link = props.link;
+
+  const visitLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    //Actual link, for counting clicks
+    window.open(process.env.NEXT_PUBLIC_API_URL+"/links/visit/"+ link.slug);
+  };
   return (
-    <Link href={link.url} className='flex-no-wrap bg-blue flex w-80 flex-col items-center overflow-hidden rounded-xl p-2' target='_blank'>
+    <Link
+        //Fake link for preview
+      href={link.url}
+      title={link.url}
+      className="flex-no-wrap bg-blue flex w-80 flex-col items-center overflow-hidden rounded-xl p-2"
+      target="_blank"
+      onClick={(e) => visitLink(e)}
+    >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       {/*have changed cover image to solid color, maybe should be dependent on the link entity*/}
       {/*      <img
