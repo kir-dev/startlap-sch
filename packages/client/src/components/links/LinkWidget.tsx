17ecd2632f74837ca9e0d5d575aa3cf5c2e3164e
Isti01 <Horvatska01@gmail.com>
2024-04-05 17:31:06 +0200

    Show trending links on client (#114)
    * Show trending links on home page
    
    * Enable caching on trending link endpoint call and fix type definitions
    
    * Set cache lifetime
    
    * Fix merge conflicts

--- packages/client/src/components/links/LinkWidget.tsx
+++ packages/client/src/components/links/LinkWidget.tsx
@@ -5,10 +5,12 @@ import '../../app/globals.css'
 import Link from "next/link";
 import React from "react";
 
+import { cn } from '@/lib/utils'
 import { LinkEntity } from '@/types/link.type'
 
 interface Props {
   link: LinkEntity
+  visits?: number
 }
 
 export default function LinkWidget(props: Props) {
@@ -37,35 +39,33 @@ export default function LinkWidget(props: Props) {
         }
         alt="felso sav"
       />*/}
-      <div className="-z-1 -mb-3 flex h-6 w-full flex-row items-center rounded-t-xl bg-red-400"></div>
+      <div className='-z-1 -mb-3 flex h-6 w-full flex-row items-center rounded-t-xl bg-red-400'></div>
 
       <div className='h-35 w-full overflow-hidden rounded-xl bg-white p-2'>
         <div className=' flex flex-grow flex-row'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            className={` ml-0 aspect-square flex-none ${
-              link.iconUrl === null ? "h-8 w-8" : "h-1/6 w-1/6"
-            }`}
+            className={cn('ml-0 aspect-square flex-none', link.iconUrl === null ? 'h-8 w-8' : 'h-1/6 w-1/6')}
             src={
               link.iconUrl === null
                 ? `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link.url.replace(
-                    "www.",
-                    ""
+                    'www.',
+                    ''
                   )}&size=128`
                 : link.iconUrl
             }
-            alt="ikon"
+            alt='ikon'
             width={100}
             height={100}
           />
-          <div className="ml-2 h-2/3 flex-grow overflow-hidden">
-            <h2 className="overflow-hidden overflow-ellipsis whitespace-nowrap text-3xl">
-              {link.title}
-            </h2>
-            <h4 className="text-xs">{link.url}</h4>
+          <div className='ml-2 h-2/3 flex-grow overflow-hidden'>
+            <h2 className='overflow-hidden overflow-ellipsis whitespace-nowrap text-3xl'>{link.title}</h2>
+            <h4 className='text-xs'>{link.url}</h4>
+            {/*We don't want to show the number of visits if it's zero*/}
+            {!!props.visits && <p className='mt-0.5 text-xs'>Látogatások száma: {props.visits}</p>}
           </div>
         </div>
-        <div className=''>
+        <div className='ml-2'>
           <p className='mt-2 overflow-hidden overflow-ellipsis whitespace-nowrap  text-base'>{link.description}</p>
         </div>
       </div>
