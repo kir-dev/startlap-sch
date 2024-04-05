1dc0190d481065a92852df64b3ffa5f3d5c485a5
Marcell Nemeth <79668706+nmarci7@users.noreply.github.com>
2024-04-05 17:06:24 +0200

    Fixed favicon staging (#113)
    * Fixed favicon scraping
    
    * Made scraped icon's size smaller

--- packages/client/src/components/links/LinkWidget.tsx
+++ packages/client/src/components/links/LinkWidget.tsx
@@ -21,15 +21,32 @@ export default function LinkWidget(props: Props) {
         }
         alt="felso sav"
       />*/}
-      <div className='-z-1 -mb-3 flex h-6 w-full flex-row items-center rounded-t-xl bg-red-400'></div>
+      <div className="-z-1 -mb-3 flex h-6 w-full flex-row items-center rounded-t-xl bg-red-400"></div>
 
       <div className='h-35 w-full overflow-hidden rounded-xl bg-white p-2'>
         <div className=' flex flex-grow flex-row'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img className='ml-0 aspect-square h-1/6 w-1/6 flex-none' src={link.iconUrl} alt='ikon' width={100} height={100} />
-          <div className='ml-2 h-2/3 flex-grow overflow-hidden'>
-            <h2 className='overflow-hidden overflow-ellipsis whitespace-nowrap text-3xl'>{link.title}</h2>
-            <h4 className='text-xs'>{link.url}</h4>
+          <img
+            className={` ml-0 aspect-square flex-none ${
+              link.iconUrl === null ? "h-8 w-8" : "h-1/6 w-1/6"
+            }`}
+            src={
+              link.iconUrl === null
+                ? `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link.url.replace(
+                    "www.",
+                    ""
+                  )}&size=128`
+                : link.iconUrl
+            }
+            alt="ikon"
+            width={100}
+            height={100}
+          />
+          <div className="ml-2 h-2/3 flex-grow overflow-hidden">
+            <h2 className="overflow-hidden overflow-ellipsis whitespace-nowrap text-3xl">
+              {link.title}
+            </h2>
+            <h4 className="text-xs">{link.url}</h4>
           </div>
         </div>
         <div className=''>
