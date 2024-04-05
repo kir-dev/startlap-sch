ca5266e5e474b1ebeb268793863d011efcea49bb
mozsarmate <mozsarmatee@gmail.com>
2024-04-05 23:34:13 +0200

    code clean up even more

--- packages/client/src/components/submission/SubmissionCard.tsx
+++ packages/client/src/components/submission/SubmissionCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 import { Button } from '@/components/ui/button'
@@ -24,57 +25,60 @@ export default function SubmissionCard({ submission }: { submission: Submission 
     }
   }
 
+  const iconSrc =
+    submission.iconUrl === null
+      ? `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${submission.url.replace('www.', '')}&size=128`
+      : submission.iconUrl
+
   return (
-    <div className='flex-no-wrap bg-blue flex w-96 flex-col items-center rounded-xl p-2'>
-      <div className={`-z-1 ${getStatusColor(submission.status)} -mb-3 flex h-10 w-full flex-row items-center rounded-t-xl p-1`}>
+    <div className='flex w-96 flex-col items-center rounded-xl p-2'>
+      <div className={`-z-1 ${getStatusColor(submission.status)} -mb-3 flex h-10 w-full items-center rounded-t-xl p-1`}>
         <h4 className='-mt-2 w-full text-center'>{submission.id}</h4>
       </div>
-      <div className='h-35 relative w-full overflow-hidden rounded-xl bg-white p-2'>
-        <div className=' flex flex-grow flex-row'>
+      <div className='relative w-full rounded-xl bg-white p-2 pb-1'>
+        <div className='ml-2 flex'>
           <img
-            className={cn('ml-0 aspect-square flex-none', submission.iconUrl === null ? 'h-8 w-8' : 'h-1/6 w-1/6')}
-            src={
-              submission.iconUrl === null
-                ? `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${submission.url.replace(
-                    'www.',
-                    ''
-                  )}&size=128`
-                : submission.iconUrl
-            }
+            className={cn('ml-0 mt-2 aspect-square flex-none', submission.iconUrl === null ? 'h-8 w-8' : 'h-1/6 w-1/6')}
+            src={iconSrc}
             alt='ikon'
             width={100}
             height={100}
           />
           <div className='ml-2 h-2/3 flex-grow'>
             <h2 className=' text-3xl'>{submission.title}</h2>
-            <h4 className='text-xs'>{submission.url}</h4>
+            <Link href={submission.url} target='_blank'>
+              <h4 className='mt-2 text-xs'>{submission.url}</h4>
+            </Link>
           </div>
         </div>
-        <div className='ml-2'>
+        <div className='ml-2 mt-2'>
           <h6>slug</h6>
-          <h4 className='mt-2 text-base'>{submission.slug}</h4>
-          <h6>desc</h6>
-          <p className='mt-2 text-base'>{submission.description}</p>
-          <h6>keywords</h6>
+          <h4 className=' text-base'>{submission.slug}</h4>
+          <h6>leírás</h6>
+          <p className='text-base'>{submission.description}</p>
+          <h6>kulcsszavak</h6>
           {submission.keywords.map((keyword: string, _index) => (
-            <div key={_index} className={'m-1 inline-block w-fit rounded bg-gray-100 px-2 py-1'}>
+            <div
+              key={_index}
+              className={'m-1 inline-block w-fit rounded bg-gray-100 px-2 py-1 ease-in-out hover:bg-gray-300 hover:transition hover:duration-100'}
+            >
               {keyword}
             </div>
           ))}
           {submission.oldLinkId && (
             <>
-              <h6>Old Link Id:</h6>
+              <h6>elavult link Id:</h6>
               <p>{submission.oldLinkId}</p>
             </>
           )}
           {submission.adminComment && submission.adminComment !== '' && (
             <>
-              <h6>admin comment</h6>
+              <h6>admin komment</h6>
               <p>{submission.adminComment}</p>
             </>
           )}
         </div>
-        <div className='my-4 flex w-full justify-evenly'>
+        <div className='my-4 flex w-full justify-end gap-2 pr-2'>
           <Button variant={'destructive'}>Elutasítás</Button>
           <Button>Jóváhagyás</Button>
         </div>
