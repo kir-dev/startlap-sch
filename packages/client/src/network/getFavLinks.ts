ff41d398942ae7e58f341e228f472698422ca0f8
Németh Marcell <nemeth.marcell.nm@gmail.com>
2024-04-05 19:13:24 +0200

    deleted console logs

--- packages/client/src/network/getFavLinks.ts
+++ packages/client/src/network/getFavLinks.ts
@@ -4,15 +4,13 @@ import api from './apiSetup'
 
 export async function getFavLinks(): Promise<{ link: LinkWithVisitsEntity[]; success: boolean }> {
   try {
-    const response = await api.get(`/user/favorites`)
+    const response = await api.get('/user/favorites')
     const rawdata = await response.data
     let success = true
     if (response.status == 401) {
       success = false
     }
-    const data = { link: rawdata, success: success }
-    console.log(data)
-    return data
+    return { link: rawdata, success: success }
   } catch (e) {
     console.error(e)
     return { link: [], success: false }
