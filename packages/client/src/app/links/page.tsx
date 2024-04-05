75d31b1528ef80927db7acfd854bc971e893d5b7
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-04-05 21:24:09 +0200

    fix: merge conflict

--- packages/client/src/app/links/page.tsx
+++ packages/client/src/app/links/page.tsx
@@ -5,32 +5,29 @@ import { TbLink } from 'react-icons/tb'
 import LinkWidget from '@/components/links/LinkWidget'
 import { Button } from '@/components/ui/button'
 import SearchField from '@/components/ui/SearchField'
-import { getLinks } from '@/network/getLinks'
-import { getProfile } from '@/network/getProfile'
+import { useProfile } from '@/hooks/queries/use-profile'
 import { useSearchLink } from '@/hooks/useSearchLink'
 
-export default async function Links() {
-  const profile = await getProfile()
-  const isAuthenticated = Boolean(profile)
-    const { links, loading, searchTerm, setSearchTerm } = useSearchLink()
-
-    return (
+export default function Links() {
+  const profile = useProfile()
+  const { links, loading, searchTerm, setSearchTerm } = useSearchLink()
+  return (
     <main>
       <div className='flex flex-col items-end'>
         <SearchField searchPhrase={searchTerm} onSubmit={setSearchTerm} />
-        <div className='flex flex-wrap justify-center gap-x-5 p-5 w-full items-start'>
+        <div className='flex w-full flex-wrap items-start justify-center gap-x-5 p-5'>
           {links.map(link => (
             <LinkWidget link={link} key={link.id} />
           ))}
         </div>
       </div>
-        {isAuthenticated && (
-            <Button asChild>
-                <Link href='/links/new'>
-                    <TbLink /> Új link
-                </Link>
-            </Button>
-        )}
+      {profile.data && (
+        <Button asChild>
+          <Link href='/links/new'>
+            <TbLink /> Új link
+          </Link>
+        </Button>
+      )}
     </main>
   )
 }
--- packages/client/src/hooks/useSearchLink.ts
+++ packages/client/src/hooks/useSearchLink.ts
@@ -1,39 +1,39 @@
-import axios from 'axios';
-import debounce from 'lodash.debounce';
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+import { useEffect, useState } from 'react'
 
-import { LinkEntity } from '@/types/link.type';
+import { LinkEntity } from '@/types/link.type'
 
 export function useSearchLink() {
-  const [filteredLinks, setFilteredLinks] = useState<LinkEntity[]>([]);
+  const [filteredLinks, setFilteredLinks] = useState<LinkEntity[]>([])
   const [loading, setLoading] = useState(true)
   const [searchTerm, setSearchTerm] = useState('')
   useEffect(() => {
     const searchLinks = async () => {
       try {
-        setLoading(true);
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/links`, { params: { term:searchTerm } });
+        setLoading(true)
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL
+        const response = await axios.get(`${apiUrl}/links`, { params: { term: searchTerm } })
         if (response.status === 200) {
-          const data = response.data;
-          setFilteredLinks(data);
+          const data = response.data
+          setFilteredLinks(data)
         } else {
-          console.error('Failed to fetch data');
+          console.error('Failed to fetch data')
         }
       } catch (error) {
-        console.error('Error:', error);
-        setFilteredLinks([]);
+        console.error('Error:', error)
+        setFilteredLinks([])
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
+    }
 
-    const debouncedSearch = debounce(searchLinks, 300);
-    debouncedSearch();
-
+    const debouncedSearch = debounce(searchLinks, 300)
+    debouncedSearch()
 
     return () => {
-      debouncedSearch.cancel();
-    };
-  }, [searchTerm]);
-  return { links: filteredLinks,loading,  searchTerm, setSearchTerm}
+      debouncedSearch.cancel()
+    }
+  }, [searchTerm])
+  return { links: filteredLinks, loading, searchTerm, setSearchTerm }
 }
