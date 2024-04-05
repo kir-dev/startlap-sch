565fb95a3ee17e0f853ebea1fefbb4e77fb6d002
mozsarmate <mozsarmatee@gmail.com>
2024-04-05 21:57:18 +0200

    valami

--- packages/client/src/app/admin/page.tsx
+++ packages/client/src/app/admin/page.tsx
@@ -1,17 +1,19 @@
-import FavLinksContainer from '@/components/links/favLinksContainer'
-import { useProfile } from '@/hooks/queries/use-profile'
+import SubmissionContainer from '@/components/submission/SubmissionContainer'
 import { getAllSubmissions } from '@/network/getAllSubmissions'
+import { getProfile } from '@/network/getProfile'
 
 export default async function Page() {
-  const user = useProfile()
+  const user = await getProfile()
   const submissions = await getAllSubmissions()
-  /*if(user.data.)*/
+
   return (
     <>
       <h1>
         <span className='italic'>zs</span>admin
       </h1>
-      <FavLinksContainer title={'A világ összes linje'} links={} />
+      {submissions.map(submission => (
+        <SubmissionContainer key={submission.id} submission={submission} />
+      ))}
     </>
   )
 }
