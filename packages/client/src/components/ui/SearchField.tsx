bd31a6bf9aa48731d962f79d94349e713a6c5e05
mozsarmate <mozsarmatee@gmail.com>
2024-04-07 21:52:12 +0200

    SearchField width fit

--- packages/client/src/components/ui/SearchField.tsx
+++ packages/client/src/components/ui/SearchField.tsx
@@ -7,7 +7,7 @@ export default function SearchField({ onSubmit, searchPhrase }: searchProps) {
   return (
     <div className='mr-8 inline-block w-fit'>
       <input
-        className='m-3 h-10 w-80 rounded border-2 border-black bg-none px-4 text-lg focus:outline-none'
+        className='m-3 h-10 w-fit rounded border-2 border-black bg-none px-4 text-lg focus:outline-none'
         type='search'
         name='search'
         placeholder='Keresés'
