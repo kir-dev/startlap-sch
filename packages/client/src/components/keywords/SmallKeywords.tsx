0d502496472e63204247ae317fae167e6c8810c6
mozsarmate <mozsarmatee@gmail.com>
2024-04-08 16:55:34 +0200

    minor changes in submission card styles, SmallKeywords.tsx init

---
+++ packages/client/src/components/keywords/SmallKeywords.tsx
@@ -0,0 +1,14 @@
+export default function SmallKeywords({ keywords }: { keywords: string[] }) {
+  return (
+    <>
+      {keywords.map((keyword: string, _index) => (
+        <div
+          key={_index}
+          className={'m-1 inline-block w-fit rounded bg-gray-100 px-2 py-1 text-xs ease-in-out hover:bg-gray-300 hover:transition hover:duration-100'}
+        >
+          {keyword}
+        </div>
+      ))}
+    </>
+  )
+}
--- packages/client/src/network/getAllSubmissions.ts
+++ packages/client/src/network/getAllSubmissions.ts
@@ -2,12 +2,19 @@ import { Submission } from '@/types/submission.type'
 
 import api from './apiSetup'
 
-export async function getAllSubmissions(): Promise<Submission[]> {
+export async function getAllSubmissions(): Promise<{ pendingSubmissions: Submission[]; reviewedSubmissions: Submission[] }> {
   try {
     const response = await api.get('/submissions')
-    return response.data
+    const pendingSubmissions: Submission[] = []
+    const reviewedSubmissions: Submission[] = []
+    const data = response.data
+    for (const submission of data) {
+      if (submission.status === 'IN_REVIEW') pendingSubmissions.push(submission)
+      else reviewedSubmissions.push(submission)
+    }
+    return { pendingSubmissions, reviewedSubmissions }
   } catch (e) {
     console.error(e)
-    return []
+    return { pendingSubmissions: [], reviewedSubmissions: [] }
   }
 }
